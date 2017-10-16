202cac4806110e4ebb0e5e4bb84c5a89f1434ae9
fredrickjomo <fredrickjomo@yahoo.com>
2017-10-16 14:40:21 +0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <link rel="stylesheet" type="text/css" href="css/homepage.css">
+	<title>Navigation Demo</title>
+</head>
+<body>
+<header>WELCOME TO THE FAMILY,We Are So Glad To Have You Here!.
+<div class="search"><input type="text" name="search" value="" placeholder="Search Orphanage" class="search-box-input" />
+<button class="search_box_button">&#128269;</button>
+</div>
+</header>
+
+<nav>
+	<ul class="showing">
+            <a href="index.php"><li>Home</li></a>
+		<a href="#"><li>About Us</li></a>
+		<a href="#"><li>Orphanages</li></a>
+		<a href="#"><li>Support</li></a>
+		<a href="#"><li>Gallery</li></a>
+		<li>
+			<a href="#">Services</a>
+			<ul class="submenu">
+				<li><a href="#"> Sponsorship</a></li>
+				<li><a href="#">Adoption</a></li>
+				<li><a href="#">Donation</a></li>
+				<li><a href="#">Charity Event(s)</a></li>
+
+
+
+			</ul>
+
+
+		</li>
+		<a href="#"><li>Contact Us</li></a>
+		<a href="#" class="donate"><li>Donate</li></a>
+
+
+
+
+	</ul>
+	<div class="handle">Menu</div>
+
+</nav>
+<div class="info">
+		<p>In the heart of every child is a hunger for home. Not just for food and a place to sleep, but for safety and community. Most importantly: for love.At the Kenyan Orphanages, found in all Kenyan parts , there are much more than a home for many Kenyan  orphans.
+		</p>	
+
+		</div>
+		
+<div class="slider">
+		
+		<img src="images/image1.jpg" >
+		<img src="images/image2.jpg" >
+		<img src="images/image3.jpg" >
+		<img src="images/image4.jpg">
+		<img src="images/image5.jpg">
+		<img src="images/image6.jpg">
+		<img src="images/image7.jpg">
+		<img src="images/image8.jpg">
+		<img src="images/image9.jpg">
+		<img src="images/image10.jpg">
+		<img src="images/image11.jpg">
+		<img src="images/image13.jpg">
+		<img src="images/image13.jpg">
+
+
+
+
+	</div>
+<section>
+In Kenya, it is estimated that there are approximately 3.6 million children aged below 18 years who have been orphaned or who are vulnerable.As per the data from the second Kenya AIDS Indicator Survey (KAIS 2012) to determine the number and profile of orphans and vulnerable children (OVC) in Kenya who were aged below 18 years that was determined.<br><br>
+Kenya AIDS Indicator Survey (KAIS 2012) estimated that there were 2.6 million OVC in Kenya in 2012, of whom 1.8 million were orphans and 750,000 were vulnerable. Among orphans, 15% were double orphans. Over one-third of all the OVC were aged between 10 and 14 years. Households with ≥1 OVC (12% of all households) were usually in the lowest 2 wealth quintiles, and 22% of OVC households had experienced moderate or severe hunger. Receipt of OVC support services was low for medical (3.7%), psychological (4.1%), social (1.3%), and material support (6.2%); educational support was slightly more common (11.5%). Orphanhood among children aged below 15 years increased from 1993 to 2003 (P < 0.01) but declined from 2003 to 2012 (P < 0.01).<br><br>
+The 2.6 million OVC constitute a significant proportion of Kenya's population aged below 18 years. Special attention should be paid to OVC to prevent further vulnerability and ensure their well-being and development as they transition into adulthood.
+</section>
+</body>
+<script type="text/javascript" src="script/jquery-3.2.1.js"></script>
+<script type="text/javascript" src="script/nav.js"></script>
+
+<footer>
+	<div class="footer-main">
+	<div class="footer-inner">
+	<div class="footer-left">
+	<div class="footer-box">
+	<h3>Services Provided</h3>
+	<ul>
+		
+<li><a href="#">Sponsorship</a></li>
+<li><a href="#">Adoption</a></li>
+<li><a href="#">Donation</a></li>
+<li><a href="#">Any Helping Service</a></li>
+
+	</ul>
+	</div>
+	<div class="footer-box">
+		<h3> Other Services</h3>
+		<ul>
+			<li><a href="#"> Help Host A Charity Event(s)</a></li>
+		</ul>
+	</div>
+	<div class="clear"> </div>
+	</div>
+	<div class="footer-box">
+	<h3>Information</h3>
+	<ul>
+		
+<li><a href="#">Orphanages</a></li>
+<li><a href="#">Carers</a></li>
+<li><a href="#">Orphan Population</a></li>
+<li><a href="#">Foreign Sponsors and Patnership Organizations</a></li>
+<li><a href="#">Support</a></li>
+
+
+	</ul>
+	</div>
+	<div class="footer-box">
+		<h3> Contact Us</h3>
+		<ul>
+			<li>Telephone: +254703646148</li>
+			<li>Email: kenyan.orphanage@kenya.co.ke</li>
+		</ul>
+
+	</div>
+
+	</div>
+	
+<div class="social_media">
+
+		<a href="#"><img class="social" src="images/social_media_icons/youtube.png"/>You Tube</a>
+		<a href="#"><img class="social " src="images/social_media_icons/facebook.png"/>Facebook</a>
+		<a href="#"><img class="social " src="images/social_media_icons/twitter.png"/>Twitter</a>
+		<a href="#"><img class="social " src="images/social_media_icons/instagram.jpg"/>Instagram</a>
+		<a href="#"><img class="social " src="images/social_media_icons/whatsapp.jpg"/>Whatsapp</a>
+
+	</div>
+</footer>
+
+
+</html>
