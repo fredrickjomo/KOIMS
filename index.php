ba530646986dc5ffd74e41ad5dd82bbabe28c54e
fredrickjomo <fredrickjomo@yahoo.com>
2017-11-20 11:21:21 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<title>Kenya Orphanage Information Management System</title>
 </head>
 <body>
-    <header><div id="header">THE KENYA ORPHANAGE HOMES.</div>
+    <header><div id="header">THE KENYA ORPHANAGE HOMES</div>
 <div class="search"><input type="text" name="search" value="" placeholder="Search Orphanage" class="search-box-input" />
 <button class="search_box_button">&#128269;</button>
 </div>
@@ -15,9 +15,8 @@
     <nav class="menu">
         <ul class="active">
             <li class="current-item"><a href="index.php"><img id="donate" src="images/menu_icons/home.png">Home</a></li>
-            <li><a href="#"><img id="donate" src="images/menu_icons/about_us.png">About Us</a></li>
-              <li><a href="#">Orphanages</a></li>
-               <li><a href="#">Gallery</a></li>
+            <li><a href="pages/about_us.php"><img id="donate" src="images/menu_icons/about_us.png">About Us</a></li>
+            <li><a href="pages/orphanages.php"><img id="donate" src="images/image4.jpg">Orphanages</a></li>
                  <li>
                      <a href="#"><img id="donate" src="images/menu_icons/support.png">Services</a>
                      <ul class="subMenu">
@@ -31,9 +30,9 @@
                  
                  </li>
                  
-                 <li><a href="#"><img id="donate" src="images/menu_icons/contact_us.png">Contact Us</a></li>
-                 <li><a href="#"><img id="donate" src="images/menu_icons/donate.jpg">Donate</div></a></li>
-                 <li><a href="#" onclick="window.open('login.php','_blank','height=400,width=500, top=200,left=300,scrollbars=no,resizable=no ')"><img id="donate" src="images/menu_icons/login-icon-png-27.png">Login</a></li>
+                 <li><a href="pages/contact_us.php"><img id="donate" src="images/menu_icons/contact_us.png">Contact Us</a></li>
+                 <li><a href="pages/donate.php"><img id="donate" src="images/menu_icons/donate.jpg">Donate</div></a></li>
+                 <li><a href="#" onclick="window.open('pages/login.php','_blank','height=400,width=500, top=200,left=300,scrollbars=no,resizable=no ')"><img id="donate" src="images/menu_icons/login-icon-png-27.png">Login</a></li>
             
             
             
@@ -43,32 +42,44 @@
 	
 
 </nav>
+    
 <div class="info">
-		<p>In the heart of every child is a hunger for home. Not just for food and a place to sleep, but for safety and community. Most importantly: for love.At the Kenyan Orphanages, found in all Kenyan parts , there are much more than a home for many Kenyan  orphans.
-		</p>	
+    <h1>OUR MISSION:</h1>
+		<p>In the heart of every child is a hunger for home. Not just for food and a place to sleep, but for safety and community. Most importantly for love.
+                    <br>At the Kenyan Orphanages, found in most of Kenyan parts , there is much more than a home for many Kenyan
+                    homeless and vulnerable orphans.
+		We are a family, where God’s love guides us all as we grow and expand year after year.<br>
+                We're working towards bringing hope to the homeless kids of kenya.Take action and help us protect the orphans by
+                sponsoring a child of your choice,making a donation,adopting, funding a project or help host a charity event(s).
+                </p>	
 
 		</div>
 		
-<div class="slider">
+                 
+<div class="slideShowContainer">
+    <ul class="slideShow">
 		
-		<img src="images/image1.jpg" >
-		<img src="images/image2.jpg" >
-		<img src="images/image3.jpg" >
-		<img src="images/image4.jpg">
-		<img src="images/image5.jpg">
-		<img src="images/image6.jpg">
-		<img src="images/image7.jpg">
-		<img src="images/image8.jpg">
-		<img src="images/image9.jpg">
-		<img src="images/image10.jpg">
-		<img src="images/image11.jpg">
-		<img src="images/image13.jpg">
-		<img src="images/image13.jpg">
+        <li> <img src="images/image1.jpg"></li>
+		<li><img src="images/image2.jpg" ></li>
+		<li><img src="images/image3.jpg" ></li>
+		<li><img src="images/image4.jpg"></li>
+		<li><img src="images/image5.jpg"></li>
+		<li><img src="images/image6.jpg"></li>
+		<li><img src="images/image7.jpg"></li>
+		<li><img src="images/image8.jpg"></li>
+		<li><img src="images/image9.jpg"></li>
+		<li><img src="images/image10.jpg"></li>
+		<li><img src="images/image11.jpg"></li>
+		<li><img src="images/image13.jpg"></li>
+		<li><img src="images/image13.jpg"></li>
+                <button class="right_arrow" onclick="plusDivs(-1)">&#10094;</button>
+                <button class="left_arrow" onclick="plusDivs(+1)">&#10095;</button>
 
 
 
-
+    </ul>
 	</div>
+               
 <section>
 In Kenya, it is estimated that there are approximately 3.6 million children aged below 18 years who have been orphaned or who are vulnerable.As per the data from the second Kenya AIDS Indicator Survey (KAIS 2012) to determine the number and profile of orphans and vulnerable children (OVC) in Kenya who were aged below 18 years that was determined.<br><br>
 Kenya AIDS Indicator Survey (KAIS 2012) estimated that there were 2.6 million OVC in Kenya in 2012, of whom 1.8 million were orphans and 750,000 were vulnerable. Among orphans, 15% were double orphans. Over one-third of all the OVC were aged between 10 and 14 years. Households with ≥1 OVC (12% of all households) were usually in the lowest 2 wealth quintiles, and 22% of OVC households had experienced moderate or severe hunger. Receipt of OVC support services was low for medical (3.7%), psychological (4.1%), social (1.3%), and material support (6.2%); educational support was slightly more common (11.5%). Orphanhood among children aged below 15 years increased from 1993 to 2003 (P < 0.01) but declined from 2003 to 2012 (P < 0.01).<br><br>
@@ -136,5 +147,5 @@ The 2.6 million OVC constitute a significant proportion of Kenya's population ag
 	</div>
 </footer>
 
-
+<div id="copyright">&COPY;2017-Kenya Orphanage Information Management System</div>
 </html>
--- scripts/home.js
+++ scripts/home.js
@@ -1,9 +1,29 @@
 $(document).ready(function(){
-   $('.toggle-nav').click(function(e){
-       
-      $(this).toggleClass('active');
-              $('.menu ul').toggleClass('active');
-                      e.preventDefault();
-   });
-    
-});3
+  var slides=$('.slideShow>li');
+  var slideCount=0;
+  var totalSlides=slides.length;
+  var slideCache=[];
+  (function preloader(){
+      if(slideCount<totalSlides){
+          //load images
+          slideCache[slideCount]=new Image();
+          slideCache[slideCount].src=slides.eq(slideCount). find('img'). attr('src');
+          slideCache[slideCount]. onload=function (){
+              slideCount++;
+                preloader();
+          }
+      }else{
+          //run the slide show
+          slideCount=0;
+          SlideShow();
+          
+      }
+  }());
+    function SlideShow(){
+        slides.eq(slideCount).fadeIn(3000).delay(5000).fadeOut(3000,function (){
+            slideCount<totalSlides-1 ? slideCount++:slideCount=0;
+            SlideShow();
+        });
+        
+    }
+});
