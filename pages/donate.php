d1f8d1dd9f9e1228f629855d691836e0c23caad0
fredrickjomo <fredrickjomo@yahoo.com>
2017-12-01 17:58:59 +0300

    Add files via upload

--- pages/donate.php
+++ pages/donate.php
@@ -6,12 +6,41 @@ and open the template in the editor.
 -->
 <html>
     <head>
+        <link rel="stylesheet" href="../css/donate.css">
         <meta charset="UTF-8">
-        <title></title>
+        <title>Donate</title>
     </head>
     <body>
+      
         <?php
         // put your code here
         ?>
+        
+        <div id="Donate">
+            <h1> &nbsp;&nbsp;&nbsp;Donate Today</h1>
+        <p>&nbsp;Help make a difference,in our society&nbsp;</p>
+        <p1><a href="">&nbsp;Donate Now&nbsp;</a></p1>
+        </div>
+        <div id="payment">
+            <h5>Choose Payment Method</h5>
+            <hr>
+            &nbsp;<a href=""><img src="../images/Donate_images/MPESA.jpg"></a>&nbsp;<br><br>
+            <hr>
+            &nbsp;<a href=""><img src="../images/Donate_images/paypal.png"></a>&nbsp;
+        </div>
+        <div id="information">
+            <h4>Help Us Support The Orphans and Vulnerable Children</h4>
+            <section>
+                Thank you for being interested in supporting the Kenyan Orphanages financially by donating.We are so 
+                grateful to have you and for supporting our move.<br>
+                Donate using any of the methods right-above by clicking on it and follow the procedures.<br><br>
+                &ldquo;Simply put,if it wasn't for faithful supporters like you , we wouldn't exist.
+                So thank you for supplying the funds that we need to make a difference
+                in the lives of orphans and most vulnerable and poor children, who struggle with eating 
+                disorders and leading miserable lives right here in our own country&rdquo;
+            </section>
+            
+        </div>
     </body>
+    
 </html>
